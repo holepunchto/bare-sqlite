2f0a5e9a5f95f88cbd7408cba5173f27f8da71ec
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-05-26 10:06:27 +0200

    Add typings (#10)

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import DatabaseSync from './lib/database-sync'
+import StatementSync from './lib/statement-sync'
+import errors from './lib/errors'
+
+export { DatabaseSync, StatementSync, errors }
---
+++ lib/database-sync.d.ts
@@ -0,0 +1,36 @@
+import StatementSync from './statement-sync'
+
+interface SQLiteDatabaseSync {
+  readonly isOpen: boolean
+
+  open(): void
+
+  close(): void
+
+  [Symbol.dispose](): void
+
+  exec(sql: string): void
+
+  prepare(sql: string): StatementSync
+
+  enableLoadExtension(allow: boolean): void
+
+  loadExtension(path: string, entryPoint?: string | null): void
+}
+
+declare class SQLiteDatabaseSync {
+  constructor(location: string, opts?: SQLiteDatabaseSync.Options)
+}
+
+declare namespace SQLiteDatabaseSync {
+  export interface Options {
+    open?: boolean
+    readOnly?: boolean
+    enableForeignKeyConstraints?: boolean
+    enableDoubleQuotedStringLiterals?: boolean
+    allowExtension?: boolean
+    timeout?: number
+  }
+}
+
+export = SQLiteDatabaseSync
