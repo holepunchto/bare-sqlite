129cef0d32d7c14fd52b5fc04c484bda5d5cc446
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-05-26 10:31:58 +0200

    Tighten formatting

--- lib/database-sync.d.ts
+++ lib/database-sync.d.ts
@@ -4,18 +4,15 @@ interface SQLiteDatabaseSync {
   readonly isOpen: boolean
 
   open(): void
-
   close(): void
 
-  [Symbol.dispose](): void
-
   exec(sql: string): void
-
   prepare(sql: string): StatementSync
 
   enableLoadExtension(allow: boolean): void
-
   loadExtension(path: string, entryPoint?: string | null): void
+
+  [Symbol.dispose](): void
 }
 
 declare class SQLiteDatabaseSync {
